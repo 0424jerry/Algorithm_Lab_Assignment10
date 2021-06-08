8c94216c4cbd64b813269f2245a132dfba7012c5
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2021-06-08 22:37:34 +0800

    Finish

--- Assignment10/main.cpp
+++ Assignment10/main.cpp
@@ -2,7 +2,9 @@
 #include<vector>
 using namespace std;
 
-bool BellmanFord(int n , int m);
+int Max = 100000000;
+
+
 
 
 
@@ -14,20 +16,45 @@ int main() {
         vector <pair<int,int>> r;
         vector<int> w;
         int x,y,t;
+
         for(int i =0 ; i < m; i++){
             cin >> x >> y >> t;
             r.push_back(make_pair(x,y));
             w.push_back(t);
         }
-        if(BellmanFord(n,m)) cout << "possible" << endl;
-        else cout << "not possible" << endl;
+
+
+        int d[n];
+        fill(d , d + n, Max);
+        d[0] = 0;
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < m;j++){
+                if(d[r[j].first] != Max){
+                    if(d[r[j].first] + w[j] < d[r[j].second]){
+                        d[r[j].second] = d[r[j].first] + w[j];
+                    }
+                }
+            }
+        }
+
+        int f = 1;
+        for(int i = 0; i < m; i++){
+            if(d[r[i].first] + w[i] < d[r[i].second]){
+                cout << "possible" << endl;
+                f = 0;
+                break;
+            }
+        }
+
+
+
+        if(f == 1)cout << "not possible" << endl;
 
 
     }
     return 0;
 }
 
-bool BellmanFord(int n , int m){
 
-}
 
